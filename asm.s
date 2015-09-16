2eab10aa8ee19682670c095178ef33622f9f6aaf
Michael Steil <mist64@mac.com>
2015-09-15 23:36:52 -0700

    more comments

--- asm.s
+++ asm.s
@@ -1,47 +1,65 @@
-; Ultimage Tron II by Oliver Stiller
+; Ultimage Tron II by Oliver Stiller 1989
+;
+; disassembled and commented by Michael Steil <mist64@mac.com> 2015
+;
 
         .setcpu "6502"
 
+; contains temporary data for explosion
 pagea := $C800
 pageb := $C900
+
+; contains a ring buffer for each player containing
+; the last 32 pixels drawn for kill detection
 history_x := $CA00
 history_y := $CB00
 
-        .byte   $00,$C0
-
         jmp     entry
 
+; "1" if the player is participating
 player_participating:
-        .byte   $01,$01,$01,$01,$01,$01
+        .byte   1,1,1,1,1,1
 
-LC009:  .byte   $00,$00,$00,$00,$00,$00
+; extra points per player, based on index of the kill
+points: .byte   0,0,0,0,0,0
 
-winner: .byte   $00
+; index of the winner
+winner: .byte   0
 
+; index of the player that escaped
 escaped_player:
-        .byte   $00
+        .byte   0
 
+; which player uses which key set
 keyset_index:
         .byte   0,2,4,6,8,10
 
+; keeps track of victims and their killers
 victim: .byte   $80,$80,$80,$80,$80,$80
 killer: .byte   $80,$80,$80,$80,$80,$80
 
-player_names: ; six player names, max. 8 chars
-        .byte   $00,$00,$00,$00,$00,$00,$00,$00
-        .byte   $00,$00,$00,$00,$00,$00,$00,$00
-        .byte   $00,$00,$00,$00,$00,$00,$00,$00
-        .byte   $00,$00,$00,$00,$00,$00,$00,$00
-        .byte   $00,$00,$00,$00,$00,$00,$00,$00
-        .byte   $00,$00,$00,$00,$00,$00,$00,$00
+; six player names, max. 8 chars
+player_names:
+        .byte   0,0,0,0,0,0,0,0
+        .byte   0,0,0,0,0,0,0,0
+        .byte   0,0,0,0,0,0,0,0
+        .byte   0,0,0,0,0,0,0,0
+        .byte   0,0,0,0,0,0,0,0
+        .byte   0,0,0,0,0,0,0,0
 
-LC053:  .byte   $00,$01,$02,$03,$04,$05
-LC059:  .byte   $00
+player_mapping:
+        .byte   0,1,2,3,4,5
 
+; counts down frames until player names are erased from the screen
+player_name_countdown:
+        .byte   0
+
+; counts number of frames a key was down
 keys_temp:
-        .byte   $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+        .byte   0,0,0,0,0,0,0,0,0,0,0,0
+; "1" if a key was detected as pressed (for long enough)
 keys_pressed:
-        .byte   $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+        .byte   0,0,0,0,0,0,0,0,0,0,0,0
 
 handle_keyboard:
         php
@@ -67,6 +85,7 @@ LC091:  ldy     keys_temp,x
         inc     keys_pressed,x ; key was stable for 2 frames -> hand to engine
         bne     LC08C
 
+; $DC00/$DC01 codes for the 12 possible control keys
 kbdtab1:
         .byte   $7F,$7F,$FB,$FD,$EF,$F7,$BF,$DF,$BF,$BF,$FE,$FE
 kbdtab2:
@@ -112,10 +131,11 @@ hirestab:
         .byte   $34,$35,$36,$37,$39,$3A,$3B,$3C
         .byte   $3E,$3F
 
+; tables for setting and clearing bits
 and_tab:.byte   $7F,$BF,$DF,$EF,$F7,$FB,$FD,$FE
 or_tab: .byte   $80,$40,$20,$10,$08,$04,$02,$01
 
-LC125:  pla ; return two levels up
+:       pla ; return two levels up
         pla
         sec ; error
         lda     #$00
@@ -126,14 +146,14 @@ LC125:  pla ; return two levels up
 ; * the lowest 3 bits of the coordinate in X and Y
 offset_from_coord:
         cpy     #200
-        bcs     LC125 ; Y exceeds coord space
+        bcs     :-    ; Y exceeds coord space
         ldx     $FD   ; X MSB
         beq     :+    ; zero is fine
         dex
-        bne     LC125 ; != 1 -> error
+        bne     :-    ; != 1 -> error
         ldx     $FC
         cpx     #320 - 256
-        bcs     LC125 ; > 320 -> error
+        bcs     :-    ; > 320 -> error
 :       pha
         lda     $FC
         and     #$F8
@@ -185,29 +205,29 @@ read_pixel:
         rts
 
 draw_border:  
-; line on the right edge of the screen
+; line on the left and right edges of the screen
         lda     #198
         sta     $FA
 :       ldy     $FA ; Y coord
-        lda     #$00
+        lda     #>1
         sta     $FD
-        lda     #$01 ; X coord = 1
+        lda     #<1 ; left
         sta     $FC
         jsr     set_or_clear_pixel ; set
         ldy     $FA
         lda     #>318
         sta     $FD
-        lda     #<318
+        lda     #<318 ; right
         sta     $FC
         jsr     set_or_clear_pixel ; set
         dec     $FA
         bne     :-
 
-; line at the bottom edge of the screen
-:       ldy     #198
+; line at the top and bottom edges of the screen
+:       ldy     #198 ; bottom
         lda     #$01
         jsr     set_or_clear_pixel ; set
-        ldy     #$01
+        ldy     #1 ; top
         tya
         jsr     set_or_clear_pixel ; set
         lda     $FC
@@ -224,8 +244,8 @@ draw_border:
         rts
 
 init_player_state:
-        ldx     #$05 ; copy tab1 into tab2
-:       lda     LC053,x
+        ldx     #5
+:       lda     player_mapping,x
         tay
         lda     start_xpositions_lo,y
         sta     xpositions_lo,x
@@ -238,7 +258,7 @@ init_player_state:
         dex
         bpl     :-
 
-        ldx     #$05
+        ldx     #5
 :       lda     player_participating,x
         sta     player_alive,x
         lda     #$80
@@ -248,6 +268,7 @@ init_player_state:
         bpl     :-
         rts
 
+; initial state
 start_xpositions_lo:
         .byte   <50,<270,<50,<270,<50,<270
 start_xpositions_hi:
@@ -258,32 +279,38 @@ start_directions:
         .byte   1,3,1,3,1,3
 
 xpositions_lo:
-        .byte   $00,$00,$00,$00,$00,$00
+        .byte   0,0,0,0,0,0
 xpositions_hi:
-        .byte   $00,$00,$00,$00,$00,$00
+        .byte   0,0,0,0,0,0
 ypositions:
-        .byte   $00,$00,$00,$00,$00,$00
+        .byte   0,0,0,0,0,0
 directions:
-        .byte   $00,$00,$00,$00,$00,$00
+        .byte   0,0,0,0,0,0
 
 addition_tab_x_lo:
-        .byte   $00,$01,$00,$FF
+        .byte   0,1,0,-1
 addition_tab_x_hi:
-        .byte   $00,$00,$00,$FF
+        .byte   0,0,0,-1
 addition_tab_y:
-        .byte   $FF,$00,$01,$00
+        .byte   -1,0,1,0
 
+; "1" if the player is participating and alive
 player_alive:
-        .byte   $00,$00,$00,$00,$00,$00
+        .byte   0,0,0,0,0,0
 
+; holds the current player while iterating over players in game_step
 current_player:
         .byte   $00
 
-LC23F:  .byte   $00
+; number of kills in this game
+kill_count:
+        .byte   $00
 
+; pointer in the history ring buffer
 history_index:
         .byte   $00
 
+; number of kills in this game (XXX redundant)
 kill_index:
         .byte   0
 
@@ -321,9 +348,9 @@ player_loop:
         ldx     current_player
         lda     #0
         sta     player_alive,x ; player died!
-        lda     LC23F
-        sta     LC009,x
-        inc     LC23F
+        lda     kill_count
+        sta     points,x
+        inc     kill_count
 ; find killer
         ldy     #32 * 6 - 1 ; 32 entries for 6 players
 find_killer_loop:
@@ -336,7 +363,7 @@ find_killer_loop:
 :       dey
         cpy     #$FF
         bne     find_killer_loop
-LC2AC:  jsr     explosion_step
+LC2AC:  jsr     explosion_init
         jmp     game_step_skip
 LC2B2:  tya
         lsr     a ; index >> 5
@@ -462,7 +489,7 @@ set_pageb:
         sta     LC3C1
         rts
 
-explosion_step:
+explosion_init:
         jsr     set_pagea
         lda     pagea + 180
         beq     LC3BF
@@ -498,7 +525,7 @@ LC3EB           := * + 2
         lda     $FD
 LC3F0           := * + 2
         sta     pagea + 20,y
-        jsr     LC413
+        jsr     get_random
         sbc     #$80
 LC3F8           := * + 2
         sta     pagea + 100,y
@@ -506,7 +533,7 @@ LC3F8           := * + 2
         sbc     #$00
 LC3FF           := * + 2
         sta     pagea + 120,y
-        jsr     LC413
+        jsr     get_random
         sbc     #$80
 LC407           := * + 2
         sta     pagea + 140,y
@@ -517,27 +544,31 @@ LC40E           := * + 2
         dey
         bpl     LC3E2
         rts
-LC413:  lda     LC421
-        adc     $D012
+
+get_random:
+        lda     rnd_seed
+        adc     $D012 ; current raster line - has some entropy
         tax
-        eor     $E74F,x
-        sta     LC421
-LC420:  rts
+        eor     $E74F,x ; a page of code in the KERNAL
+        sta     rnd_seed
+rts0:   rts
 
-LC421:  .byte 0
+rnd_seed:
+        .byte 0
 
-LC422:  jsr     set_pagea ; run the sequence above twice, with pagea and pageb
+explosion_step:
+        jsr     set_pagea ; run the sequence above twice, with pagea and pageb
         jsr     LC42B
         jsr     set_pageb
 LC42B:
 LC42D           := * + 2
         lda     pagea + 180
-        beq     LC420 ; rts
+        beq     rts0
         lda     #0 ; flag to clear pixel
         jsr     LC470
 LC437           := * + 2
         dec     pagea + 180
-        beq     LC420
+        beq     rts0
         ldy     #19
 LC43C:
 LC43E           := * + 2
@@ -577,7 +608,7 @@ LC46A           := * + 2
         lda     #1 ; flag to set pixel
 LC470:  pha
         ldx     #19
-        stx     LC493
+        stx     explosion_index
 LC476:
 LC478           := * + 2
         lda     pagea + 0,x
@@ -591,13 +622,14 @@ LC482           := * + 2
         pla
         pha
         jsr     set_or_clear_pixel
-        dec     LC493
-        ldx     LC493
+        dec     explosion_index
+        ldx     explosion_index
         bpl     LC476
         pla
         rts
 
-LC493:  .byte 0
+explosion_index:
+        .byte 0
 
 ; the main program
 ; * draws the playing field
@@ -607,12 +639,12 @@ LC493:  .byte 0
 entry:  jsr     init_screen
         jsr     init_player_state
         jsr     draw_border
-        jsr     show_player_names
-        lda     #$00
-        sta     LC23F
+        jsr     show_or_hide_player_names ; show player names
+        lda     #0
+        sta     kill_count
         sta     $DC0E ; stop CIA1 Timer A
-        lda     #$14
-        sta     LC059
+        lda     #20
+        sta     player_name_countdown ; show player names for another 20 frames
         lda     #$80
         sta     escaped_player
         lda     #$3B
@@ -670,23 +702,27 @@ end_game:
         cli
         rts
 
+; the core of the game runs in sync with the raster beam
 irq:    lda     $D019 ; ACK raster IRQ
         sta     $D019
-        lda     LC059
-        bmi     LC530
-        dec     LC059
-        bne     LC530
-        jsr     show_player_names
-LC530:  jsr     game_step
+        lda     player_name_countdown
+        bmi     :+
+        dec     player_name_countdown
+        bne     :+
+        jsr     show_or_hide_player_names ; erase player names
+:       jsr     game_step
         jsr     handle_input
-        jsr     LC422
+        jsr     explosion_step
         jmp     $EA81 ; KERNAL RTI code
 
-LC53C:  .byte   $D0,$B0,$50,$30,$D0,$B0
-LC542:  .byte   $23,$24,$2B,$2C,$32,$33
+; memory offsets that point to the location in hires where names are shown
+hiresoff_lo:
+        .byte   <$23D0,<$24B0,<$2B50,<$2C30,<$32D0,<$33B0
+hiresoff_hi:
+        .byte   >$23D0,>$24B0,>$2B50,>$2C30,>$32D0,>$33B0
 
-show_player_names:
-        ldx     #$00
+show_or_hide_player_names:
+        ldx     #0
         sei
         lda     #$33 ; enable char ROM at $D000
         sta     $01
@@ -695,16 +731,16 @@ LC54F:  txa
         lsr     a
         lsr     a
         tay
-        lda     LC053,y
+        lda     player_mapping,y
         tay
-        lda     LC53C,y
+        lda     hiresoff_lo,y
         sta     $FE
-        lda     LC542,y
+        lda     hiresoff_hi,y
         sta     $FF
         lda     #$08
         sta     $FB
 LC566:  lda     player_names,x
-        beq     show_player_names_skip
+        beq     show_or_hide_player_names_skip
         sec
         sbc     #$20
         asl     a
@@ -714,12 +750,12 @@ LC566:  lda     player_names,x
         lda     #$D0
         sta     $F9
         ldy     #$07
-:       lda     ($F8),y
-        eor     ($FE),y
-        sta     ($FE),y
+:       lda     ($F8),y ; XOR them onto the screen,
+        eor     ($FE),y ; so if this is called twice,
+        sta     ($FE),y ; they will be removed
         dey
         bpl     :-
-show_player_names_skip:
+show_or_hide_player_names_skip:
         lda     $FE
         clc
         adc     #$08
@@ -733,9 +769,6 @@ show_player_names_skip:
         cpx     #$30
         bcc     LC54F
         lda     #$37
-        sta     $01
-        cli
-        rts
-
+        sta     $01 ; restore memory mapping
         cli
         rts
